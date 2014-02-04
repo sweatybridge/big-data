2646c0de14af4af9a2a2f05fd6c7c5879d6fe745
Qiao Han <sweatybridge@gmail.com>
2014-02-04 13:34:53 +0000

    combined carousel page, added anchor and smooth scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// smooth scrolling
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
 // data maps
 var map = new Datamap({
     element: document.getElementById('map'),
