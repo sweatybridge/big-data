f9428821f20509e4cf3bbf11eb1d4864326d9f85
Qiao Han <sweatybridge@gmail.com>
2014-02-26 02:45:52 +0000

    replaced navigation top bar with side bar; added parallax scrolling effect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,53 @@
 'use strict';
 
-// smooth scrolling
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+// Scroll the background layers
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg1').css('top',(0-(scrolled*0.25))+'px');
+    $('#parallax-bg2').css('top',(0-(scrolled*0.5))+'px');
+    $('#parallax-bg3').css('top',(0-(scrolled*0.75))+'px');
+}
+
+// Set navigation dots to an active state as the user scrolls 
+function redrawDotNav(){
+    var section1Top =  0;
+    // The top of each section is offset by half the distance to the previous section.
+    var section2Top =  $('#about').offset().top - ($(window).height() / 2);
+    var section3Top =  $('#demo').offset().top - ($(window).height() / 2);
+    $('nav#primary a').removeClass('active');
+    if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
+        $('nav#primary a.home').addClass('active');
+    } else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
+        $('nav#primary a.about').addClass('active');
+    } else if ($(document).scrollTop() >= section3Top){
+        $('nav#primary a.demo').addClass('active');
+    }
+}
+
+$(document).ready(function() {
+    redrawDotNav();
+    // Scroll event handler 
+    $(window).bind('scroll', function() {
+        parallaxScroll();
+        redrawDotNav();
+    });
+
+    // Show/hide dot lav labels on hover
+    $('nav#primary a').hover(function() {
+        $(this).prev('h1').show();
+    }, function() {
+        $(this).prev('h1').hide();
+    });
+
+    // smooth scrolling
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500, function() {
+            parallaxScroll(); // Callback is required for iOS
+        });
+        return false;
+    });
 });
 
 // data maps
